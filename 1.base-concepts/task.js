c85e8139bd8c5efc745dea85f9f81db081c3db4a
Sergey Karpaev <34karpaev@mail.ru>
2024-04-29 15:49:31 +0500

    доработка первого задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,30 +1,24 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-	let arr = [];
-	let discriminant = b ** 2 - 4 * a * c;
-	if (discriminant < 0) {
-		return arr;
-	} else if (discriminant === 0) {
-		arr.push(-b / (2 * a));
-	} else if (discriminant > 0) {
-		arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
-		arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
-	}
-	return arr;
+  let arr = [];
+  let discriminant = b ** 2 - 4 * a * c;
+  if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+  }
+  return arr;
 }
 console.log(solveEquation(2, 3, 1))
 
 
-let percent;
-let contribution;
-let amount;
-let countMonths;
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  percent = percent / 100 / 12;
-  let monthlyPayment = amount - contribution;
-  let payment = monthlyPayment * (percent + (percent / (((1 + percent) ** countMonths) - 1)))
-  let summa = parseFloat(payment * countMonths);
-  return summa.toFixed(2);
-}
-console.log(calculateTotalMortgage(10, 0, 50000, 12));
+	let monthlyPercent = percent / 100 / 12;
+	let monthlyPayment = amount - contribution;
+	let payment = monthlyPayment * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)))
+	let summa = payment * countMonths;
+	return Number(summa.toFixed(2));
+  }
+  console.log(calculateTotalMortgage(10, 0, 50000, 12));
